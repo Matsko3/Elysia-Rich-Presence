cf59f58503ab7e96c0ae49d1c1eba43001e345c9
Matsko3 <deathgun5852@gmail.com>
2023-06-04 07:08:21 +0300

    amogus

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def is_star_rail_running():
 
 # Wait for StarRail.exe launch
 while not is_star_rail_running():
-    print("Waiting for StarRail.exe to be running...")
+    print("\x1b[38;5;91mWaiting for StarRail.exe to be running...\x1b[38;5;91m")
     time.sleep(5)
     if not gw.getWindowsWithTitle("StarRail"):
         print("StarRail.exe is not running.")
@@ -24,10 +24,44 @@ process_id = subprocess.run(['tasklist', '/fi', 'IMAGENAME eq StarRail.exe', '/f
 process_lines = process_id.splitlines()
 if len(process_lines) > 1:
     process_id = process_lines[1].split(',')[1].strip('"')
-    print("StarRail.exe found with process ID:", process_id)
-    print("Starting the Rich Presence")
+    print("\x1b[38;5;91mStarRail.exe found with process ID:\x1b[38;5;212m", process_id)
+    print("\x1b[38;5;212mStarting the Rich Presence\x1b[38;5;212m")
+    print("                                ╥▄")
+    print("                               ╓██▌")
+    print("                               █▓▓█")
+    print("                             ╓█▓▓▓█")
+    print("                            ▄█▓█▓▓█")
+    print("                         ▄██████▓█▀")
+    print("          ▀██    ▄╖   ┌██████████▌ ╓╖╥╦╦╦╦╦▄╗╦╦╦╗╖╖╓")
+    print("           ▀██▄  ▐▓▓ ▄██████████▀╜▒╓╥▄▄▓ ░░▒▒▒╙  ╙░╙╜▀╬╥")
+    print("            ▀██─ █▒╢█████████▀▀▀▒╜▀▓▓█▀    ╙          ╙▒▒▓╗")
+    print("             ███╒▓▒╢████████▄▒▄▓▓███▀▄▓▓▓▓╖   ╓╬▓▓╗╖     ╙▒╙╗")
+    print("             ▐███▒▒▓██▀╙╙▀▀████▀▀░▄▓▓▓╜╜╜╜    ╙╜╨▓▓▓▓▓╖      ╙╦")
+    print("             ▐██▒║▒██▌╦╬▓▓██▓▄▄┐╩╜   ░             ░ ╙▓▓╗      ╙╗")
+    print("             ▐██░▒▒█▀▒▒▒▒▒▒▄████████▄▄╓         ╙╗      ╫▓╖     └▓")
+    print("             ▐██▌▄▓▒▒▒▓▓██████████████▀░░        ╙╣  ░    ▓╗      ▓")
+    print("              ╙███░▒▓▓████████████▀┌   ░░░        ║┐ ╙╢    ╙░    ░ ▓")
+    print("                 █▄║▓██▀╢▒▀▀▀▀░╟╣     ░      ╗    ╠▓▄  ╟╗        ░░ ╙╦       ╓")
+    print("                  ████▒╜  ╓╣▒ ▓▌   ░         ▓   ░╠▓▒▓  ║▒╖  ░    ╙╖   ╙╨╤═╦▓")
+    print("                 ╟███╜░   ╫▒░╟▓▒  ░░        ┌▓  ▒░╟▌ ╟▓  ╢▒▒ ░    ░░╙▓╥╥╥╣╜")
+    print("                ▐▌▐█▌└░  ░▓▒░▓▓▒  ░░   ░ ░ ░╠▓░ ╢▒▓▒  ╠▌ ╟▒▒▒▒░      ╙╗")
+    print("               ╔▓╢╣╜░░░   ▓▒╫╣▓▒ ░░ ░░▒░ ░ ▒▓▓░╓▒╟▓    ▓╖╠▓▓▒╫░ ░░  ▒ ╫")
+    print("              ╒▓▒╢▓░░░░░░ ╟╣▓▒▓▒ ░░  ╟▒ ░ ▒▒▓╢▒╢╫▓     ╠▓╟▓╟╣╣  ░░ ░╫▒ ▓")
+    print("              ▓╫╩▀▓╣░░░░░░▒╢▓▒╙▓▓▓▓▓▓▓╣╬▓▓▓▀╙╨╫▓╜  ╥▓▓▓▓╣╢░▒▓▒░░▒░░▒▓╝╝╨▀┐")
+    print("             ╟ ╙▓╖ ╟▒▒░░░▒▒▒▒▒ ╠╬╬╬╬╬╬            ║▓╬╬╬▓▓  ╟╣░░▒░ ▒╫▒ ╓╦╜╗")
+    print("             ╬ ╟▓▓▓▓▓▒▒╖░░╙▒╣░  ░░░░░░             ░╓╥╝▒  ╓╣╖▒▒░░▒▒▓▓╝▓▒░╙╖")
+    print("            ╓▒╓▌▓╢▒╙╫▓▒▒▒▒▒╖▒╫╗  ▒░░░              ░░▒▒  ╓▒▒╣▒╥▒▒▒▓╜░╓▓▒  ▓")
+    print("            ╟▒▓  ╚▓▒░▒▓▓▒▒▒▒▒▒▒▓╖                      ╓▓▓▒▒▒▒▒╫▓░░░░▓ ╟ ░▓")
+    print("             ▒╢    ╟╖▒░░░╙╨▓▓▒▒▒▒╨╨╜      ╥╖╖╖─    ╨▓▓▒▒▒▒▒▄▓▓╜░░░░╓▓  ╣ ▓")
+    print("             ╟▒     ╙╣▒▒▒░▒▒╙▓▓▓▓▓▄╖             ╖  ╓▄▓▓▓▓▓▀░░░░▒░╬╜  ╥╬▓")
+    print("              ╫╖      ╫╣▓╖░▒░░░▒╙▓▓▓▓▓▓▄▄▄╥╦╥╦▄▄▄▓▒▀▀▀╜╙░░▒░░░▒░╫▓▓▓▓▓▓")
+    print("               ╙╬╖      ▓▓▓╩╬▒░▒▒▒╥╣▓█████▓███▓███▓▓╖░░▒▒▒░▒▒▓▓▀╙╙╙")
+    print("               ╥  ╙    ╓╣▒╜▒▒▒╢▓▓╣▓██▓▓█████████████▓▓▒╜▓▒▒╙▒▒▒▓╗      ╓▒")
+    print("                ╙▓▄╬▓▓▒▒▒╥╣╣╣╢╢╬╩▀╖███████▓███████▓▓█▌█▓▓╣▓╬╢╫╬╣╣╣▓▓▓▓▓╙")
+    print("                    ╙╜╜╨╜╜╙        ▀███▓▓▀█▀█▀▀   ▀╩╩▀▀▌     ╙╙╜╨╜╜╙\x1b[38;5;212m")
+
 else:
-    print("Unable to retrieve process ID for StarRail.exe")
+    print("\x1b[38;5;91mUnable to retrieve process ID for StarRail.exe\x1b[38;5;91m")
     sys.exit()
 
 # Read the config JSON file
@@ -39,61 +73,78 @@ with open('config/configs.json', 'r') as file:
 with open('config/fallback.json', 'r') as file:
     refresh_data = json.load(file)
 
-# Initialize/Start the Discord RPC client
-RPC = Presence('724484541825286205')
+history = []
 
-RPC.connect()
+# handling exceptions and restarting the script
+def handle_exception():
+    print("An exception occurred. Restarting the script...")
+    time.sleep(5)  # Wait for a few seconds before restarting
 
-# Auto refreshing data
-refresh_details = refresh_data['refresh_details']
-refresh_state = refresh_data['refresh_state']
-refresh_large_image = refresh_data['refresh_large_image']
-refresh_small_image = refresh_data['refresh_small_image']
-
-start_time = int(time.time())
-
-# Main loop
 while True:
-    # Capture the screen text
-    screen_image = ImageGrab.grab()
+    try:
+        # Initialize/Start the Discord RPC client
+        RPC = Presence('724484541825286205')
+        RPC.connect()
 
-    # Use OCR to extract text from the image
-    extracted_text = extract_text_from_image(screen_image)
+        # Auto refreshing data
+        refresh_details = refresh_data['refresh_details']
+        refresh_state = refresh_data['refresh_state']
+        refresh_large_image = refresh_data['refresh_large_image']
+        refresh_small_image = refresh_data['refresh_small_image']
 
-    # Initialize variables for Rich Presence data
-    details = refresh_details
-    state = refresh_state
-    large_image = refresh_large_image
-    small_image = refresh_small_image
+        start_time = int(time.time())
 
-    # Check if word matches the extracted text
-    for word_data in words_to_search:
-        word_lines = word_data['word'].split('\n')
-        if all(line in extracted_text for line in word_lines):
-            details = word_data['details']
-            state = word_data['state']
-            large_image = word_data['large_image']
-            small_image = word_data['small_image']
-            break  # Exit the loop once a match is found
+        # Main loop
+        while True:
+            try:
+                # Capture the screen text
+                screen_image = ImageGrab.grab()
 
-    # Updating the Rich Presence activity
-    RPC.update(
-        details=details,
-        state=state,
-        large_image=large_image,
-        small_image=small_image,
-        start=start_time
-    )
+                # Use OCR to extract text from the image
+                extracted_text = extract_text_from_image(screen_image)
 
-    # Printing Rich Presence data
-    print("Rich Presence Updated:")
-    print("Details:", details)
-    print("State:", state)
-    print("Large Image:", large_image)
-    print("Small Image:", small_image)
-    print("-----------------------")
+                # Initialize variables for Rich Presence data
+                details = refresh_details
+                state = refresh_state
+                large_image = refresh_large_image
+                small_image = refresh_small_image
 
-    # Sleep for some time before capturing the next text
-    time.sleep(4)
+                # Check if word matches the extracted text
+                for word_data in words_to_search:
+                    word_lines = word_data['word'].split('\n')
+                    if all(line in extracted_text for line in word_lines):
+                        details = word_data['details']
+                        state = word_data['state']
+                        large_image = word_data['large_image']
+                        small_image = word_data['small_image']
+                        break  # Exit the loop once a match is found
 
-RPC.close()
+                # Updating the Rich Presence activity
+                RPC.update(
+                    details=details,
+                    state=state,
+                    large_image=large_image,
+                    small_image=small_image,
+                    start=start_time
+                )
+
+                # Printing Rich Presence data
+                print("\x1b[38;5;91mRich Presence Updated:\x1b[38;5;91m")
+                print("Details:", details)
+                print("State:", state)
+                print("Large Image:", large_image)
+                print("Small Image:", small_image)
+                print("-----------------------\x1b[38;5;91m")
+                # Sleep for some time before capturing the next text
+                time.sleep(4)
+
+            except Exception as e:
+                handle_exception()
+
+    except Exception as e:
+        handle_exception()
+
+    finally:
+        # Close the Rich Presence connection before restarting
+        if 'RPC' in locals() and RPC is not None:
+            RPC.close()
